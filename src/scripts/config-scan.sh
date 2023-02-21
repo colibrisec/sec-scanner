be841ef67a2c56eff7b5c34f8b66ae5f79496d88
James Luther <james@b34rd.tech>
2023-02-21 16:41:56 -0500

    updates

--- src/scripts/config-scan.sh
+++ src/scripts/config-scan.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-trivyLatest=$(curl --silent "https://api.github.com/repos/aquasecurity/trivy/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-trivyLink=$(echo "$trivyLatest" | cut -d 'v' -f 2)
 
-FILE=trivy_"${trivyLink}"_Linux-64bit.tar.gz
-if [ ! -f "$FILE" ]; then
-  wget https://github.com/aquasecurity/trivy/releases/download/"$trivyLatest"/trivy_"${trivyLink}"_Linux-64bit.tar.gz
-  tar zxvf trivy_"${trivyLink}"_Linux-64bit.tar.gz
-fi
 ./trivy config -f json -o results-config.json .
