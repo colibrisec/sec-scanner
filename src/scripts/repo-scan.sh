eb13ef27304347aa8838fd62b904da2fcb586776
James Luther <james@b34rd.tech>
2023-02-21 16:49:02 -0500

    updates

--- src/scripts/repo-scan.sh
+++ src/scripts/repo-scan.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+trivyLatest=$(curl --silent "https://api.github.com/repos/aquasecurity/trivy/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+trivyLink=$(echo "$trivyLatest" | cut -d 'v' -f 2)
 
+FILE=trivy_"${trivyLink}"_Linux-64bit.tar.gz
+if [ ! -f "$FILE" ]; then
+  wget https://github.com/aquasecurity/trivy/releases/download/"$trivyLatest"/trivy_"${trivyLink}"_Linux-64bit.tar.gz
+  tar zxvf trivy_"${trivyLink}"_Linux-64bit.tar.gz
+fi
 if [ "${PARAM_IGNOREUNFIXED}" == true ]; then
-  trivy fs -f json -o results-repo.json --severity "${PARAM_SEVERITY}" --ignore-unfixed .
+  ./trivy fs -f json -o results-repo.json --severity "${PARAM_SEVERITY}" --ignore-unfixed .
 else
-  trivy fs -f json -o results-repo.json --severity "${PARAM_SEVERITY}" .
+  ./trivy fs -f json -o results-repo.json --severity "${PARAM_SEVERITY}" .
 fi
