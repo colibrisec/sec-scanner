503b4c6cb27fd15abb44ccd392603159883cd4e4
James Luther <james@b34rd.tech>
2023-02-21 16:53:52 -0500

    fixed bugs in web-scan

--- src/scripts/web-scan.sh
+++ src/scripts/web-scan.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+zapLatest=$(curl --silent "https://api.github.com/repos/zaproxy/zaproxy/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+zapLink=$(echo "$zapLatest" | cut -d 'v' -f 2)
 
 FILE=ZAP_"${zapLink}"_Linux.tar.gz
 if [ ! -f "$FILE" ]; then
