5a6670808c18f861898f092e0ab0db5c58671756
James Luther <james@b34rd.tech>
2023-02-21 16:16:31 -0500

    update web-scan

--- src/scripts/web-scan.sh
+++ src/scripts/web-scan.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 zapLatest=$(curl --silent "https://api.github.com/repos/zaproxy/zaproxy/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 zapLink=$(echo "$zapLatest" | cut -d 'v' -f 2)
+FOLDER=ZAP_"${zapLink}"
 
 FILE=ZAP_"${zapLink}"_Linux.tar.gz
 if [ ! -f "$FILE" ]; then
   wget https://github.com/zaproxy/zaproxy/releases/download/"$zapLatest"/ZAP_"${zapLink}"_Linux.tar.gz
   tar zxvf "$FILE"
 fi
-./zap.sh -quickurl "${PARAM_URL}" -quickprogress -quickout zapreport.json
+cd "${FOLDER}"
+./zap.sh -quickurl "${PARAM_URL}" -quickprogress -quickout ../zapreport.json
