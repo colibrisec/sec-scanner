f216810848e70df84a7983310fe79c4e8976167d
James Luther <james@b34rd.tech>
2023-02-21 16:43:35 -0500

    update trivy scanners

--- src/scripts/config-scan.sh
+++ src/scripts/config-scan.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./trivy config -f json -o results-config.json .
+trivy config -f json -o results-config.json .
--- src/scripts/repo-scan.sh
+++ src/scripts/repo-scan.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "${PARAM_IGNOREUNFIXED}" == true ]; then
-  ./trivy fs -f json -o results-repo.json --severity "${PARAM_SEVERITY}" --ignore-unfixed .
+  trivy fs -f json -o results-repo.json --severity "${PARAM_SEVERITY}" --ignore-unfixed .
 else
-  ./trivy fs -f json -o results-repo.json --severity "${PARAM_SEVERITY}" .
+  trivy fs -f json -o results-repo.json --severity "${PARAM_SEVERITY}" .
 fi
