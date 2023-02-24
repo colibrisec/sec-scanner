1d7491ed3edfdb7507832ac78b769f3f9c0007a6
James Luther <james@b34rd.tech>
2023-02-24 15:52:54 -0500

    updated tests

--- src/scripts/cosign.sh
+++ src/scripts/cosign.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-echo "Working"
+cosignLatest=$(curl --silent "https://api.github.com/repos/zaproxy/zaproxy/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+
+FILE=cosign-linux-amd64
+if [ ! -f "$FILE" ]; then
+  wget https://github.com/zaproxy/zaproxy/releases/download/"$cosignLatest"/cosign-linux-amd64
+  tar zxvf "$FILE"
+fi
+
+# Implement futher to store signed vuln scans / sboms
+
+ls
+echo "Signing ${PARAM_TYPE}"
--- src/scripts/report.sh
+++ src/scripts/report.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-echo "Working"
+
+# Report to where?
+# Currently supporting Postee, DefectDojo, and SonarQube
+#
+
+if [ "${PARAMETER_SERVER}" == "defectdojo" ]; then
+  # defectdojo
+  curl -X POST "${PARAM_URL}" -H "Authorization: Bearer " "${PARAM_TOKEN}" --data "${PARAM_REPORT}"
+fi
+
+if [ "${PARAMETER_SERVER}" == "sonarqube" ]; then
+  # sonarqube
+  curl -X POST "${PARAM_URL}" -H "Authorization: Bearer " "${PARAM_TOKEN}" --data "${PARAM_REPORT}"
+fi
+
+if [ "${PARAMETER_SERVER}" == "postee" ]; then
+  # postee
+  curl -X POST "${PARAM_URL}" -H "Authorization: Bearer " "${PARAM_TOKEN}" --data "${PARAM_REPORT}"
+fi
